76ca9f4b7ff8ba37fdb4bb61729562352f05ca72
ArjunDharvath14 <arjundharvath14@gmail.com>
2025-10-05 10:52:14 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express=require('express');
+const connectDB=require("./config/database");
+const User=require("./models/user");
 const app=express();
-const {adminAuth}=require("./middlewares/Auth");
-app.use("/userData",(req,res)=>{
-    throw new Error;
+app.post("/signup", async(req,res)=>{
+    const user=new User({
+        firstName:"Virat",
+        lastName:"kohli",
+        emailId:"viratKohli@gmail.com",
+        password:"virat@123"
+    });
+     await user.save();
+     res.send("User Added SuccessFully");
+})
 
-})
-app.use("/",(err,req,res,next)=>{
-    if(err)
-    {
-        res.status(500).send("Something went wrong");
-    }
-})
-
-app.listen(1814,()=>{
-    console.log("Server is Successfully listening on port 1814");
-})
+connectDB()
+.then(()=>{console.log("DataBase Connection Established");
+    app.listen(1814,()=>{
+    console.log("Server is Successfully listening on port 1814");})})
+.catch((err)=>{console.error("Database is not connected")});
