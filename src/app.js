7a9e248b25bd45b6ffc3308b296207a538b146f2
ArjunDharvath14 <arjundharvath14@gmail.com>
2025-10-06 21:37:01 +0530

    created jwt token and put it inside cookie and got user profile by cookie

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const connectDB=require("./config/database");
 const User=require("./models/user");
 const {validateSignUpData}=require("./utils/validation");
 const bcrypt=require('bcrypt');
+const CookieParser=require('cookie-parser');
+const jwt=require('jsonwebtoken');
 const app=express();
 app.use(express.json());
+app.use(CookieParser());
 app.post("/signup", async(req,res)=>{
     try{
         validateSignUpData(req);
@@ -21,6 +24,46 @@ app.post("/signup", async(req,res)=>{
         res.status(404).send("Error:"+err.message);
      }
 });
+app.post("/login",async (req,res) => {
+    try{
+        const {emailId,password}=req.body;
+        const user= await User.findOne({emailId:emailId});
+        if(!user)
+        {
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid)
+        {
+            throw new Error("Invalid Credentials");
+        }
+        const token=jwt.sign({_id:user._id},"DEV@TINDER14");
+        res.cookie("token",token);
+        res.send("Login SuccessFul");
+    }
+    catch(err){
+        res.status(404).send("Error:"+err.message);
+     }
+})
+app.get("/profile",async(req,res)=>{
+    try{const cookie=req.cookies;
+    const {token}=cookie;
+    if(!token)
+    {
+        throw new Error("Token is not valid");
+    }
+    const decodedMessage=await jwt.verify(token,"DEV@TINDER14");
+    const {_id}=decodedMessage;
+    const user= await User.findById(_id);
+    if(!user)
+    {
+        throw new Error("User does not exist");
+    }
+    res.send(user);}
+    catch(err){
+        res.status(404).send("Error:"+err.message);
+     }
+})
 app.delete("/user", async(req,res)=>{
     try{
         const userId=req.body.userId;
